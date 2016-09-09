6ad1b557704388b7f0f9f7cf622fb5b89d8035e4
labxray <labxray@D-10-155-95-38.dhcp4.washington.edu>
2016-09-09 17:22:44 -0400

    Update install.sh and automate installation of anaconda

--- install.sh
+++ install.sh
@@ -2,23 +2,33 @@
 #Install various software
 dir=~/dotfiles
 
-git config --global core.excludesfile '~/.gitignore'
-
-#cd ~/.vim
-git submodule update --init --recursive
+bash copyconfig.sh
 
 # Install ranger if necessary
 cd ranger
 python setup.py install --optimize=1 --record=install_log.txt --user
 
-# Install fasd
-cd ~/.clvv-fasd-4822024
-PREFIX=$HOME make install
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+git config --global core.excludesfile '~/.gitignore'
+
+#cd ~/.vim
+git submodule update --init --recursive
+
+# If python isn't installed on this system, then download and install anaconda
+if ! command_exists python ; then
+	wget https://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh
+	bash Anaconda2-4.1.1-Linux-x86_64.sh -b -p $HOME/anaconda3
+	export PATH="$HOME/anaconda3/bin:$PATH"
+fi
 
 # Make directory for vim undo history
 mkdir -p $HOME/.vimundo
+mkdir -p $HOME/.local/bin
 # Install par
-sudo dnf -y install par the_silver_searcher
+sudo dnf -y install par the_silver_searcher gcc-c++
 
 # Install fzf
 ~/.fzf/install
@@ -27,5 +37,6 @@ sudo dnf -y install par the_silver_searcher
 cd ~/.vim/bundle/YCM
 ./install.py --clang-completer &
 
-cd $dir
-.copyconfig.sh
+# Install fasd
+cd ~/.clvv-fasd-4822024
+PREFIX=$HOME make install
