d03dede5182e973b4943c488c29af9142adde2b2
OpsWorks user ohoidn <ohoidn@jupyter-aws.prod.use1>
2018-02-14 22:55:26 +0000

    Tweaked vim build; tested on Amazon Linux
    Added shell function gg that runs git grep with .gitignored files
    excluded.

--- viminstall.sh
+++ viminstall.sh
@@ -2,23 +2,29 @@
 #sudo ln -s /usr/bin/xsubpp /usr/share/perl5/ExtUtils/xsubpp
 
 # TODO: steer this from the main install script
+# TODO: distro-dependent build dependencies for lua
 PYTHON_CONFIG=$HOME/anaconda2/lib/python2.7/config
-VIMDIR=$HOME/.local/usr/share/vim/vim80/
+VIMDIR=$HOME
+#VIMDIR=$HOME/.local/usr/share/vim/vim80/
 
 cd ~
 git clone https://github.com/vim/vim.git
 cd vim
 make distclean
 
+
 # Check ~/vim/src/auto/config.log for config issues
-CFLAGS+="-O -fPIC -Wformat" ./configure --with-features=huge \
+CFLAGS+="-O -fPIC -Wformat" ./configure \  --with-features=huge \
             --prefix=$VIMDIR \
             --enable-multibyte \
-            --enable-rubyinterp \
-            --enable-pythoninterp \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
             --with-python-config-dir=$PYTHON_CONFIG \
-            --enable-perlinterp \
-            --enable-luainterp \
+            --enable-luainterp=yes \
+            --enable-luajit=yes \
             --enable-gui=gtk2 --enable-cscope
-make -j 6
-make install VIMRUNTIMEDIR=$VIMDIR DESTDIR=~/.local
+	    --enable-fail-if-missing
+	    --with-lua-prefix=/usr/include/
+            #--enable-perlinterp \
+make -j 8
+make install #DESTDIR=~/.local #VIMRUNTIMEDIR=$VIMDIR DESTDIR=~/.local
