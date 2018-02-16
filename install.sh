07b9f6bb12de2084a11e529d070ffeafcef41ee4
OpsWorks user ohoidn <ohoidn@jupyter-aws.prod.use1>
2018-02-16 00:22:10 +0000

    Fix tig installer; add script par.sh to install par from source.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ command_exists () {
 
 git config --global alias.tree "log --oneline --decorate --all --graph"
 
-# sudo dnf -y install par the_silver_searcher gcc-c++ clang-devel tmux vim-enhanced
+# sudo dnf -y install the_silver_searcher gcc-c++ clang-devel tmux vim-enhanced
 
 mkdir -p $HOME/bin
 #cd ~/.vim
--- tig.sh
+++ tig.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-source env.sh
-git submodule update --init --recursive
+#source env.sh
+#git submodule update --init --recursive
 # Install tig
-cd $dir/tig
+cd tig
 (make > tig.log 2>&1 && make install >> tig.log 2>&1 ) &
