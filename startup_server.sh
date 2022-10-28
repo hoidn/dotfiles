7a40f3903fb6f96b9a9a2e5878841ce24fc3687b
O H <ohoidn@slac.stanford.edu>
2022-10-27 18:33:43 -0700

    add system startup script

--- startup_server.sh
+++ startup_server.sh
@@ -6,3 +6,9 @@ tmux send-keys -t autossh2 "autossh -M 20002 -R 47.147.134.169:8888:localhost:88
 
 tmux new-session -d -s autossh3 'bash -l'
 tmux send-keys -t autossh3 "autossh -M 20007 -R 47.147.134.169:3307:localhost:3306 pi@47.147.134.169" ENTER
+
+tmux new-session -d -s autossh4 'bash -l'
+tmux send-keys -t autossh4 "autossh -M 20008 -R 47.147.134.169:51006:localhost:51005 pi@47.147.134.169" ENTER
+
+tmux new-session -d -s autossh5 'bash -l'
+tmux send-keys -t autossh5 "autossh -M 20009 -R 47.147.134.169:32401:localhost:8096 pi@47.147.134.169" ENTER
