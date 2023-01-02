d94aeb873188f7935584c24d0f324c3038022918
O H <ohoidn@slac.stanford.edu>
2023-01-01 19:45:22 -0500

    add hdd mount

--- startup_server.sh
+++ startup_server.sh
@@ -1,3 +1,4 @@
+sudo mount /dev/sda1 /media/
 tmux new-session -d -s autossh1 'bash -l'
 tmux send-keys -t autossh1 "autossh  -M 20004 -R 47.147.134.169:2222:localhost:22 pi@47.147.134.169" ENTER
 
@@ -16,8 +17,11 @@ tmux send-keys -t autossh5 "autossh -M 20009 -R 47.147.134.169:32401:localhost:8
 tmux new-session -d -s autossh6 'bash -l'
 tmux send-keys -t autossh6 "autossh -M 20010 -R 47.147.134.169:6006:localhost:6006 pi@47.147.134.169" ENTER
 
+tmux new-session -d -s autossh9 'bash -l'
+tmux send-keys -t autossh9 "autossh -M 20011 -R 47.147.134.169:8889:localhost:8889 pi@47.147.134.169" ENTER
+
 # from https://gist.github.com/proudlygeek/5721498
-tmux new-session -d -s autossh7 'bash -l'
-tmux send-keys -t autossh7 "autossh -M 20011 -L 3049:localhost:2049 pi@47.147.134.169" ENTER
+tmux new-session -d -s autossh8 'bash -l'
+tmux send-keys -t autossh8 "autossh -M 20012 -L 3049:localhost:2049 pi@47.147.134.169" ENTER
 #ssh -L 3049:localhost:2049 pi@47.147.134.169
 #sudo mount -t nfs -o port=3049 localhost:/media/pi/ff9e1ec0-72b7-4574-b12d-66f4a792d1fb/nfs /mnt/nfs-share
