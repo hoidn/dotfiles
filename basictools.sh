0dbb09ce76be582fc5394641848f3f3f498f8c71
OH <ohoidn@slac.stanford.edu>
2023-03-02 19:04:22 -0500

    script for renaming incremental backup directories

--- basictools.sh
+++ basictools.sh
@@ -1,9 +1,11 @@
+sudo apt install fzf fasd socat
 # Make directory for vim undo history
 mkdir -p $HOME/.vimundo
 mkdir -p $HOME/.local/bin
 
 bash copyconfig.sh
 echo "copied configs"
+
 
 mkdir -p $HOME/bin
 #cd ~/.vim
