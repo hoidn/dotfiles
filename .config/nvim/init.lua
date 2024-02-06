5dc0a4ab4f0c1afc66384de53bc744ed1df39917
O H <ohoidn@slac.stanford.edu>
2024-02-05 21:55:17 -0500

    set up LSP and python completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,78 +1,145 @@
 vim.cmd [[
+set path=$PWD/**
+
 call plug#begin('~/.local/share/nvim/plugged')
 
 Plug 'junegunn/fzf.vim'
 Plug 'junegunn/fzf'
+Plug 'junegunn/seoul256.vim'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'MunifTanjim/nui.nvim'
 Plug 'dpayne/CodeGPT.nvim'
 Plug '/home/ollie/Documents/magma-nvim', { 'do': ':UpdateRemotePlugins' }
 Plug 'neomake/neomake'
 Plug 'ggandor/leap.nvim'
+Plug 'neovim/nvim-lspconfig'
+
+Plug 'hrsh7th/cmp-nvim-lsp-signature-help'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+"Plug 'hrsh7th/cmp-vsnip' -- For snippet support
+"Plug 'hrsh7th/vim-vsnip' -- Snippet engine
+
 autocmd! VimEnter * lua require('leap').set_default_keymaps()
 
-
 call plug#end()
-]]
 
--- Set the path to include the current working directory
-vim.opt.path:append('**')
+let mapleader=" "
 
--- Set the clipboard to use the unnamed register
-vim.opt.clipboard:prepend('unnamed')
+" map , to toggle fold
+nnoremap , za
 
--- Make the undo history persistent
-vim.opt.undofile = true
+imap jk <Esc>
+" Match default binding in Spacemacs
+imap fd <Esc>
+map <leader>h :wincmd h<CR>
+map <leader>j :wincmd j<CR>
+map <leader>k :wincmd k<CR>
+map <leader>l :wincmd l<CR>
 
--- Set a directory to store the undo history
-vim.opt.undodir = vim.fn.expand('~/.vimundo')
 
--- Set backspace behavior
-vim.opt.backspace = {'indent', 'eol', 'start'}
+" Map ctrl-s to save in normal and insert mode
+nmap <C-s> :w<CR>
+imap <c-s> <Esc>:w<CR>
+map <leader>q :q<CR>
+map <leader>w :w<CR>
 
--- Set the format program
-vim.opt.formatprg = 'par'
+" multiline edit shortcut
+ noremap <leader>n :norm 
+ noremap <leader>i :norm i#<CR>
+ autocmd Filetype python vmap <leader>c :norm i#<CR>
 
--- Disable polyglot vim syntax for Python
-vim.g.polyglot_disabled = {'python'}
+" toggle line number
+ noremap <leader>N :set nu!<CR>
 
--- Unified color scheme (default: dark)
--- vim.cmd('colorscheme seoul256')
+" Toggle showing invisible characters
+"map <leader>s :set list!<CR>
+map <leader>e :split<CR>
+map <leader>v :vsplit<CR>
 
--- Switch background to dark
-vim.opt.background = 'dark'
--- Custom functions and autocommands for Neovim
+" Call norm ex command in visual mode
+vmap <leader>n :norm 
 
--- Function to search the visual selection with Ag
-vim.api.nvim_exec([[
-function SearchVisualSelectionWithAg() range
-  let old_reg = getreg('"')
-  let old_regtype = getregtype('"')
-  let old_clipboard = &clipboard
-  set clipboard&
-  normal! ""gvy
-  let selection = getreg('"')
-  call setreg('"', old_reg, old_regtype)
-  let &clipboard = old_clipboard
-  execute 'Ag' selection
+" un-highlight search results
+nmap <leader>/ :nohlsearch<CR>
+
+noremap ]<leader> :lnext<CR>
+noremap [<leader> :lprevious<CR>
+
+let g:fzf_action = {
+  \ 'ctrl-t': 'tab split',
+  \ 'ctrl-x': 'split',
+  \ 'ctrl-v': 'vsplit' }
+
+
+" Command for git grep
+" - fzf#vim#grep(command, with_column, [options], [fullscreen])
+command! -bang -nargs=* GGrep
+  \ call fzf#vim#grep('git grep --line-number '.shellescape(<q-args>), 0, <bang>0)
+
+function! SearchVisualSelectionWithAg() range
+	let old_reg = getreg('"')
+	let old_regtype = getregtype('"')
+	let old_clipboard = &clipboard
+	set clipboard&
+	normal! ""gvy
+	let selection = getreg('"')
+	call setreg('"', old_reg, old_regtype)
+	let &clipboard = old_clipboard
+	execute 'Ag' selection
 endfunction
-vnoremap K :call SearchVisualSelectionWithAg()<CR>
-]], false)
+"vnoremap  K :call SearchVisualSelectionWithAg()<CR>
 
--- Function to open Ranger file manager
-vim.api.nvim_exec([[
 function RangerExplorer()
-  exec "silent !ranger --choosefile=/tmp/vim_ranger_current_file " . expand("%:p:h")
-  if filereadable('/tmp/vim_ranger_current_file')
-      exec 'edit ' . system('cat /tmp/vim_ranger_current_file')
-      call system('rm /tmp/vim_ranger_current_file')
-  endif
-  redraw!
-endfunction
-]], false)
+    exec "silent !ranger --choosefile=/tmp/vim_ranger_current_file " . expand("%:p:h")
+    if filereadable('/tmp/vim_ranger_current_file')
+        exec 'edit ' . system('cat /tmp/vim_ranger_current_file')
+        call system('rm /tmp/vim_ranger_current_file')
+    endif
+    redraw!
+endfun
+" Enter ranger file manager
+nmap <Leader>x :call RangerExplorer()<CR>
 
--- Autocommands for setting file-specific options
-vim.api.nvim_exec([[
+"TODO: debug missing fzf#vim#grep
+"let g:NERDTreeHijackNetrw = 0 // add this line if you use NERDTree
+" // open ranger when vim open a directory
+"let g:ranger_replace_netrw = 1
+
+" Allow saving of files as sudo when I forgot to start vim using sudo.
+cmap w!! w !sudo tee > /dev/null %
+
+set clipboard=unnamed
+
+
+" Disable jedi completions in favor of YCM (we still want jedi semantic completions though)
+let g:jedi#completions_enabled = 1
+let g:jedi#use_splits_not_buffers = "bottom"
+let g:pymode_rope = 0
+
+" Jedi
+"autocmd FileType python setlocal completeopt-=preview
+" fix for neocomplete
+"let g:jedi#popup_select_first = 0
+"let g:jedi#popup_on_dot = 0
+
+" neocomplete
+" use TAB completion neocomplete
+"inoremap <expr><TAB>  pumvisible() ? "\<C-n>" : "\<TAB>"
+"let g:neocomplete#enable_at_startup = 1
+"" Set minimum syntax keyword length.
+"let g:neocomplete#sources#syntax#min_keyword_length = 3
+
+" jedi key mappings
+autocmd Filetype python vnoremap <leader>d "_d
+"autocmd Filetype python vnoremap <leader>p "_dP
+autocmd Filetype rc vmap <leader>c :norm i"<CR>
+vmap <leader>x :norm x<CR>
+
+syntax on
 autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4
 autocmd Filetype c setlocal expandtab tabstop=4 shiftwidth=4
 autocmd Filetype cpp setlocal expandtab tabstop=4 shiftwidth=4
@@ -80,247 +147,152 @@ au BufRead,BufNewFile *.cc setfiletype cpp
 autocmd Filetype h setlocal expandtab tabstop=4 shiftwidth=4
 autocmd Filetype hpp setlocal expandtab tabstop=4 shiftwidth=4
 autocmd Filetype tex setlocal expandtab tabstop=2 shiftwidth=2
-]], false)
-
--- Additional custom key mappings
-vim.api.nvim_set_keymap('v', 'J', ':m \'>+1<CR>gv=gv', {noremap = true})
-vim.api.nvim_set_keymap('v', 'K', ':m \'<-2<CR>gv=gv', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>p', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', {noremap = true})
-
--- Additional settings
--- ...
--- Set leader key to a space
-vim.g.mapleader = " "
-
--- Map ',' to toggle fold
-vim.api.nvim_set_keymap('n', ',', 'za', {noremap = true})
-
--- Map 'jk' to escape in insert mode
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap = true})
-
--- Map 'fd' to escape in insert mode (Match default binding in Spacemacs)
-vim.api.nvim_set_keymap('i', 'fd', '<Esc>', {noremap = true})
-
--- Window navigation mappings using the leader key
-vim.api.nvim_set_keymap('n', '<leader>h', ':wincmd h<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>j', ':wincmd j<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>k', ':wincmd k<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>l', ':wincmd l<CR>', {noremap = true})
-
--- Map ctrl-s to save in normal and insert mode
-vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', {noremap = true})
-vim.api.nvim_set_keymap('i', '<C-s>', '<Esc>:w<CR>', {noremap = true})
-
--- Multiline edit shortcuts
-vim.api.nvim_set_keymap('v', '<leader>n', ':norm ', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>i', ':norm i#<CR>', { noremap = true, silent = true })
-
--- Map leader key with 'q' to quit and 'w' to save
-vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', {noremap = true})
-
--- Toggle line number with leader key and 'N'
-vim.api.nvim_set_keymap('n', '<leader>N', ':set nu!<CR>', {noremap = true})
-
--- Split window mappings using the leader key
-vim.api.nvim_set_keymap('n', '<leader>e', ':split<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>v', ':vsplit<CR>', {noremap = true})
-
--- Un-highlight search results with leader key and '/'
-vim.api.nvim_set_keymap('n', '<leader>/', ':nohlsearch<CR>', {noremap = true})
-
--- Navigation through location list with leader key and brackets
-vim.api.nvim_set_keymap('n', ']<leader>', ':lnext<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '[<leader>', ':lprevious<CR>', {noremap = true})
-
--- Map leader key with 'L' to list files using fzf
-vim.api.nvim_set_keymap('n', '<leader>L', ':call fzf#run({\'source\': \'git ls-files\', \'sink\': \'e\'})<CR>', {noremap = true, silent = true})
-
--- Map leader key with 'F' to search files using fzf
-vim.api.nvim_set_keymap('n', '<leader>F', ':Files<CR>', {noremap = true, silent = true})
-
--- Map leader key with 'u' to list buffers using fzf
-vim.api.nvim_set_keymap('n', '<leader>u', ':Buffers<CR>', {noremap = true, silent = true})
-
--- Map leader key with 'f' to locate files
-vim.api.nvim_set_keymap('n', '<leader>f', ':Locate ', {noremap = true})
-
--- Map leader key with 'H' to show command history
-vim.api.nvim_set_keymap('n', '<leader>H', ':History:<CR>', {noremap = true})
-
--- Map leader key with 'c' to show recently opened files
-vim.api.nvim_set_keymap('n', '<leader>c', ':History<CR>', {noremap = true, silent = true})
-
--- Map leader key with 'a' to search text using Ag
-vim.api.nvim_set_keymap('n', '<leader>a', ':Ag<CR>', {noremap = true, silent = true})
--- Plugin settings for Neovim
-
-local has_xsel = vim.fn.executable("xsel") == 1
-
-if has_xsel then
-  vim.g.clipboard = {
-    name = "xsel",
-    copy = {
-      ["+"] = "xsel --input --clipboard",
-      ["*"] = "xsel --input --primary",
-    },
-    paste = {
-      ["+"] = "xsel --output --clipboard",
-      ["*"] = "xsel --output --primary",
-    },
-    cache_enabled = 0,
-  }
-end
-
--- Python host program
-vim.g.python3_host_prog = '/home/ollie/anaconda3/envs/tf/bin/python3'
-
--- Pathogen
-vim.cmd('call pathogen#infect()')
-
--- Syntastic
-vim.g.syntastic_always_populate_loc_list = 1
-vim.g.syntastic_mode_map = { mode = 'passive', active_filetypes = {'python'} }
-vim.g.syntastic_python_checkers = {'pyflakes'}
-
--- YouCompleteMe (YCM)
--- vim.g.loaded_youcompleteme = 1 -- Uncomment this line to disable YCM
-vim.g.ycm_server_keep_logfiles = 1
-vim.g.ycm_server_log_level = 'debug'
-vim.g.ycm_extra_conf_vim_data = {'&filetype'}
-vim.g.ycm_autoclose_preview_window_after_insertion = 0
--- YCM key mappings
-vim.api.nvim_exec([[
-  autocmd Filetype cpp noremap <leader>d :YcmCompleter GoTo<CR>
-  autocmd Filetype c noremap <leader>d :YcmCompleter GoTo<CR>
-  autocmd Filetype python noremap <leader>f :YcmCompleter GoTo<CR>
-]], false)
-
--- FZF
-vim.g.fzf_action = {
-  ['ctrl-t'] = 'tab split',
-  ['ctrl-x'] = 'split',
-  ['ctrl-v'] = 'vsplit'
-}
--- FZF commands
-vim.cmd([[
-  command! -bang -nargs=* GGrep
-    \ call fzf#vim#grep('git grep --line-number '.shellescape(<q-args>), 0, <bang>0)
-]])
-
----- Ranger
---vim.api.nvim_exec([[
---  function RangerExplorer()
---    exec "silent !ranger --choosefile=/tmp/vim_ranger_current_file " . expand("%:p:h")
---    if filereadable('/tmp/vim_ranger_current_file')
---        exec 'edit ' . system('cat /tmp/vim_ranger_current_file')
---        call system('rm /tmp/vim_ranger_current_file')
---    endif
---    redraw!
---  endfun
---  nmap <Leader>x :call RangerExplorer()<CR>
---]], false)
-
--- Slime
-vim.g.slime_target = 'tmux'
-vim.g.slime_python_ipython = 1
-
--- Disable jedi completions in favor of YCM
-vim.g.jedi_completions_enabled = 1
-vim.g.jedi_use_splits_not_buffers = 'bottom'
-vim.g.pymode_rope = 0
--- Jedi key mappings
-vim.api.nvim_exec([[
-  autocmd Filetype python vnoremap <leader>d "_d
-]], false)
-
--- Disable polyglot vim syntax
-vim.g.polyglot_disabled = {'python'}
-
--- Persistent undo history
-vim.opt.undofile = true
-vim.opt.undodir = vim.fn.expand('~/.vimundo')
-
--- Format program
-vim.opt.formatprg = 'par'
-
--- Color scheme
-vim.cmd('colorscheme seoul256')
-vim.opt.background = 'dark'
-
--- Additional key mappings for fzf-vim
--- ...
-
--- Additional settings
--- ...
+"
 
 
--- Terminal mode escape mapping
-vim.api.nvim_set_keymap('t', 'jk', '<C-\\><C-n>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+let g:syntastic_always_populate_loc_list = 1
+" Configure syntastic for python
+let g:syntastic_mode_map = {
+    \ "mode": "passive",
+        \ "active_filetypes": ["python"] }
+let g:syntastic_python_checkers = ['pyflakes']
 
--- Neoterm settings
-vim.g.neoterm_position = 'vertical'
-vim.api.nvim_set_keymap('n', '<C-c><C-c>', 'vip:TREPLSendSelection<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-c><C-x>', 'vip:TREPLSendLine<CR>', {noremap = true})
+	
+let g:ycm_server_keep_logfiles = 1
+let g:ycm_server_log_level = 'debug'
 
--- Neomake settings
-vim.g.neomake_python_enabled_makers = {'pyflakes'}
-vim.api.nvim_create_autocmd('BufWritePost', {
-    pattern = '*',
-    command = 'Neomake'
+" YCM clang key mappings
+"let g:ycm_autoclose_preview_window_after_completion=1
+let g:ycm_extra_conf_vim_data = ['&filetype']
+autocmd Filetype cpp noremap <leader>d :YcmCompleter GoTo<CR>
+autocmd Filetype c noremap <leader>d :YcmCompleter GoTo<CR>
+autocmd Filetype python noremap <leader>f :YcmCompleter GoTo<CR>
+
+
+"let g:ycm_confirm_extra_conf = 0
+"let g:ycm_global_ycm_extra_conf = '~/.vim/.ycm_extra_conf.py'
+
+" Have YCM populate the location list with errors
+"let g:ycm_always_populate_location_list = 1
+
+" Autoclose preview window
+let g:ycm_autoclose_preview_window_after_insertion = 0
+
+
+" Make the undo history persistent
+set undofile
+" set a directory to store the undo history
+set undodir=~/.vimundo
+
+set formatprg=par
+
+" Disable polyglot vim syntax
+let g:polyglot_disabled = ['python']
+
+" Extra key bindings for fzf-vim
+"
+
+
+" TODO: think about using this:
+"function! s:ag_in(...)
+"  call fzf#vim#ag(join(a:000[1:], ' '), extend({'dir': a:1}, g:fzf#vim#default_layout))
+"endfunction
+"
+"command! -nargs=+ -complete=dir AgIn call s:ag_in(<f-args>)
+
+"
+"nnoremap <silent> K :call SearchWordWithAg()<CR>
+"nnoremap <silent> <leader>gl :Commits<CR>
+"nnoremap <silent> <leader>ga :BCommits<CR>
+"nnoremap <silent> <leader>ft :Filetypes<CR>
+
+" bind ctrl-d to fzf tag search in c files
+" see https://github.com/junegunn/fzf.vim/issues/323
+" nnoremap <leader>d :call fzf#vim#tags(expand('<cword>'), {'options': '--exact --select-1 --exit-0'})<CR>
+nnoremap <leader>L :call fzf#run({'source': 'git ls-files', 'sink': 'e'})<CR>
+
+" TODO: debug the following:
+"let g:rg_command = '
+"  \ rg --column --line-number --no-heading --fixed-strings --ignore-case --no-ignore --hidden --follow --color "always"
+"  \ -g "*.{js,json,php,md,styl,jade,html,config,py,cpp,c,go,hs,rb,conf}"
+"  \ -g "!{.git,node_modules,vendor}/*" '
+"
+"command! -bang -nargs=* F call fzf#vim#grep(g:rg_command .shellescape(<q-args>), 1, <bang>0)
+" git grep
+"command! -bang -nargs=* GGrep
+"  \ call fzf#vim#grep('git grep --line-number '.shellescape(<q-args>), 0, <bang>0)<Paste>
+
+" Default fzf layout
+" - down / up / left / right
+" - window (nvim only)
+let g:fzf_layout = { 'down': '~40%' }
+
+nnoremap <silent> <leader>F :Files<CR>
+nnoremap <silent> <leader>u :Buffers<CR>
+nnoremap <leader>f :Locate 
+" nnoremap <leader>w :Windows<CR>
+" vim command history
+nnoremap <leader>H :History:<CR>
+" recently opened files
+nnoremap <leader>c :History<CR>
+" "nnoremap <silent> <leader>; :BLines<CR>
+" "nnoremap <silent> <leader>o :BTags<CR>
+" "nnoremap <silent> <leader>O :Tags<CR>
+" "nnoremap <silent> <leader>? :History<CR>
+" "nnoremap <silent> <leader>/ :execute 'Ag ' . input('Ag/')<CR>
+nnoremap  <leader>a :Ag<CR>
+
+let g:slime_target = "tmux"
+" use the %cpaste function in Python buffers
+let g:slime_python_ipython = 1
+
+set backspace=indent,eol,start " make backspace work over linebreaks, etc.
+
+" Unified color scheme (default: dark)
+colo seoul256
+
+" Light color scheme
+"colo seoul256-light
+
+" Switch
+set background=dark
+
+vnoremap J :m '>+1<CR>gv=gv
+vnoremap K :m '<-2<CR>gv=gv
+"vnoremap <leader>p "_dP
+nnoremap <leader>p :%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>
+]]
+
+require'lspconfig'.pyright.setup{}
+
+local cmp = require'cmp'
+
+-- https://chat.openai.com/share/f317e520-9de4-4d11-9cd8-469162f139fc
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For snippets
+  }, {
+    { name = 'buffer' },
+  })
 })
 
--- Magma setup
-vim.g.magma_automatically_open_output = false
-vim.g.magma_image_provider = "ueberzug"
-
-local function MagmaInitPython()
-    vim.cmd(':MagmaInit python3')
-    vim.cmd(':MagmaEvaluateArgument a=5')
-end
-
-local function MagmaInitCSharp()
-    vim.cmd(':MagmaInit .net-csharp')
-    vim.cmd(':MagmaEvaluateArgument Microsoft.DotNet.Interactive.Formatting.Formatter.SetPreferredMimeTypesFor(typeof(System.Object),"text/plain");')
-end
-
-local function MagmaInitFSharp()
-    vim.cmd(':MagmaInit .net-fsharp')
-    vim.cmd(':MagmaEvaluateArgument Microsoft.DotNet.Interactive.Formatting.Formatter.SetPreferredMimeTypesFor(typeof<System.Object>,"text/plain")')
-end
-
-vim.cmd('command MagmaInitPython lua MagmaInitPython()')
-vim.cmd('command MagmaInitCSharp lua MagmaInitCSharp()')
-vim.cmd('command MagmaInitFSharp lua MagmaInitFSharp()')
-
--- Magma key mappings
-vim.api.nvim_set_keymap('n', '<LocalLeader>r', ':MagmaEvaluateOperator<CR>', {silent = true, noremap = true})
-vim.api.nvim_set_keymap('n', '<LocalLeader>rr', ':MagmaEvaluateLine<CR>', {silent = true, noremap = true})
-vim.api.nvim_set_keymap('x', '<LocalLeader>r', ':<C-u>MagmaEvaluateVisual<CR>', {silent = true, noremap = true})
-vim.api.nvim_set_keymap('n', '<LocalLeader>rc', ':MagmaReevaluateCell<CR>', {silent = true, noremap = true})
-vim.api.nvim_set_keymap('n', '<LocalLeader>rd', ':MagmaDelete<CR>', {silent = true, noremap = true})
-vim.api.nvim_set_keymap('n', '<LocalLeader>ro', ':MagmaShowOutput<CR>', {silent = true, noremap = true})
-   -- Window navigation mappings
-vim.api.nvim_set_keymap('t', '<A-h>', '<C-\\><C-n><C-w>h', {noremap = true})
-vim.api.nvim_set_keymap('t', '<A-j>', '<C-\\><C-n><C-w>j', {noremap = true})
-vim.api.nvim_set_keymap('t', '<A-k>', '<C-\\><C-n><C-w>k', {noremap = true})
-vim.api.nvim_set_keymap('t', '<A-l>', '<C-\\><C-n><C-w>l', {noremap = true})
-vim.api.nvim_set_keymap('n', '<A-h>', '<C-w>h', {noremap = true})
-vim.api.nvim_set_keymap('n', '<A-j>', '<C-w>j', {noremap = true})
-vim.api.nvim_set_keymap('n', '<A-k>', '<C-w>k', {noremap = true})
-vim.api.nvim_set_keymap('n', '<A-l>', '<C-w>l', {noremap = true})
+-- Setup lspconfig.
+require('lspconfig')['pyright'].setup {
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
+}
 
 
----- Function to set terminal background color
---local function set_terminal_background()
---    vim.api.nvim_set_hl(0, 'Normal', { bg = 'black' })
---end
---
----- Autocommand to change background color in terminal buffers
---vim.api.nvim_create_autocmd('TermOpen', {
---    pattern = 'term://*',
---    callback = set_terminal_background
---})
---
---
+
