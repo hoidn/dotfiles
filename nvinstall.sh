65e84a624c4a75732ed671230ad4960bc785f613
Oliver Hoidn <ohoidn@sdfiana002.sdf.slac.stanford.edu>
2024-02-06 22:50:00 -0800

    update neovim install script to build from source

--- nvinstall.sh
+++ nvinstall.sh
@@ -11,16 +11,20 @@
 #	cd ../
 #fi
 #
-#cd neovim
+cd neovim
 #make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=$HOME/neovim"
-#make install
+make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local"
+make install
 #pip install neovim -U
-#ln -s ~/neovim/bin/nvim ~/.local/bin/
+ln -s ~/neovim/bin/nvim ~/.local/bin/
 
-sudo apt-get install neovim
-# X11 headers are needed to build / install ueberzug. libxres-dev, in particular, isn't mentioned in most of the
-# build instructions online
-sudo apt-get install libx11-dev
-sudo apt-get install libxext-dev
-sudo apt-get install libxres-dev
-pip3 install pynvim ueberzug 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#sudo apt-get install neovim
+## X11 headers are needed to build / install ueberzug. libxres-dev, in particular, isn't mentioned in most of the
+## build instructions online
+#sudo apt-get install libx11-dev
+#sudo apt-get install libxext-dev
+#sudo apt-get install libxres-dev
+#pip3 install pynvim ueberzug 
