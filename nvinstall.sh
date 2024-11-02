70114de35f170ae0563bfb091f327bf17b53f1ec
O H <ohoidn@slac.stanford.edu>
2024-11-01 23:12:14 -0400

    fix: neovim installation and configuration issues
    Several issues were encountered and fixed while installing neovim from source:
    
    1. Build permissions:
       - Previous build artifacts were owned by root, preventing new builds
       - Fixed by cleaning build dir before rebuilding
    
    2. Binary location:
       - CMAKE_INSTALL_PREFIX set to ~/.local for correct installation path
       - Removed incorrect symlink creation since make install handles it
       - Fixed stale bash hash table entry with 'hash -r'
    
    3. Config loading:
       - Added symlink from dotfiles/.config/nvim to ~/.config/nvim
       - Ensures configs and plugins are properly loaded
    
    The install script now properly handles permissions, paths, and config
    linking for a clean neovim installation.

--- nvinstall.sh
+++ nvinstall.sh
@@ -1,30 +1,56 @@
-#command_exists () {
-#    type "$1" &> /dev/null ;
-#}
-#
-#if ! command_exists cmake ; then
-#	cd cmake
-#	./bootstrap 
-#	make
-#	make install
-#	sudo make install
-#	cd ../
-#fi
-#
 cd neovim
-#make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=$HOME/neovim"
+
+# Clean previous build artifacts owned by root
+sudo rm -rf build
+
+# Build and install to ~/.local
 make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local"
 make install
-#pip install neovim -U
-ln -s ~/neovim/bin/nvim ~/.local/bin/
 
+# Create symlink after verifying binary exists
+mkdir -p ~/.local/bin
+if [ -f ~/.local/bin/nvim ]; then
+    echo "Neovim binary successfully installed"
+else
+    echo "ERROR: Neovim binary not found at ~/.local/bin/nvim after install"
+    exit 1
+fi
+
+# Setup config directory
+mkdir -p ~/.config
+ln -sf ~/dotfiles/.config/nvim ~/.config/
+
+# Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-#sudo apt-get install neovim
-## X11 headers are needed to build / install ueberzug. libxres-dev, in particular, isn't mentioned in most of the
-## build instructions online
-#sudo apt-get install libx11-dev
-#sudo apt-get install libxext-dev
-#sudo apt-get install libxres-dev
-#pip3 install pynvim ueberzug 
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+##command_exists () {
+##    type "$1" &> /dev/null ;
+##}
+##
+##if ! command_exists cmake ; then
+##	cd cmake
+##	./bootstrap 
+##	make
+##	make install
+##	sudo make install
+##	cd ../
+##fi
+##
+#cd neovim
+##make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=$HOME/neovim"
+#make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local"
+#make install
+##pip install neovim -U
+#[ -f ~/.local/bin/nvim ] && rm ~/.local/bin/nvim
+#ln -s ~/neovim/bin/nvim ~/.local/bin/
+#
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#
+##sudo apt-get install neovim
+### X11 headers are needed to build / install ueberzug. libxres-dev, in particular, isn't mentioned in most of the
+### build instructions online
+##sudo apt-get install libx11-dev
+##sudo apt-get install libxext-dev
+##sudo apt-get install libxres-dev
+##pip3 install pynvim ueberzug 
